8b3c0065227e9fd603ec266061149cec8598a2e4
Utsavladia <utsavladia@gmail.com>
2024-01-01 22:05:25 +0530

    improved commands and response of alan ai

--- src/App.js
+++ src/App.js
@@ -1,66 +1,89 @@
-import { useEffect ,useState} from "react";
-import alanBtn from '@alan-ai/alan-sdk-web';
+import { useEffect, useState, useRef } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 import "./index.css";
 import NewsCards from "./components/NewsCards";
-import DefaultNewsCards from "./components/DefaultNewsCards"
-import axios from 'axios';
+import DefaultNewsCards from "./components/DefaultNewsCards";
+import axios from "axios";
 import Navbar from "./components/Navbar";
 import CategoryCards from "./components/CategoryCards";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 function App() {
   const [articles, setArticles] = useState([]);
-
+  const alanBtnRef = useRef({}).current;
+  const newsapikey = process.env.REACT_APP_NEWS_API_KEY;
 
   useEffect(() => {
-
-    axios.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=8389f4795e7f4535bfaeae36bb60a410')
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=in&apiKey=${newsapikey}`
+      )
       .then((response) => {
         setArticles(response.data.articles);
       })
       .catch((error) => {
-        console.error('Error fetching news:', error);
+        console.error("Error fetching news:", error);
       });
 
+    alanBtnRef.btnInstance = alanBtn({
+      key: process.env.REACT_APP_ALAN_KEY,
 
-    alanBtn({
-      key: '4e0e6c32e4834da58a2f29062e59483a2e956eca572e1d8b807a3e2338fdd0dc/stage',
-        onCommand: ({command, articles}) => {
-
-          if (command === 'newHeadlines') {
-            setArticles(articles);
-
-            // Call the client code that will react to the received command
+      onCommand: async ({ command, category }) => {
+        if (command === "categories") {
+          try {
+            const response = await axios.get(
+              `https://newsapi.org/v2/top-headlines?country=in&apiKey=${newsapikey}&category=${category}`
+            );
+            setArticles(response.data.articles);
+          } catch (error) {
+            console.log("error fetching categgory", category, error);
           }
 
+          // Call the client code that will react to the received command
+          // } else if (command === "confirmation" && articles) {
+          //   console.log("Received confirmation command with articles:", articles);
+          //   const articlesText = articles
+          //     .map((article) => article.title)
+          //     .join(". "); // Concatenate titles
+          //   alanBtnRef.btnInstance.playText(
+          //     `Hi, these are the article titles: ${articlesText}`
+          //   );
+        } else if (command === "topic") {
+          try {
+            const response = await axios.get(
+              `https://newsapi.org/v2/top-headlines?country=in&apiKey=${newsapikey}&q=${category}`
+            );
+            setArticles(response.data.articles);
+          } catch (error) {
+            console.log("returned with the error", error);
+          }
         }
+      },
     });
   }, []);
-
-
 
   return (
     <div className="App">
       <Navbar />
       <NewsCards />
-      <DefaultNewsCards articles={articles}/>
+      <DefaultNewsCards articles={articles} />
       <section id="Business">
-        <CategoryCards category={'business'}/>
+        <CategoryCards category={"business"} />
       </section>
       <section id="Entertainment">
-        <CategoryCards category={'entertainment'}/>
+        <CategoryCards category={"entertainment"} />
       </section>
       <section id="Health">
-        <CategoryCards category={'health'}/>
+        <CategoryCards category={"health"} />
       </section>
       <section id="Technology">
-        <CategoryCards category={'technology'}/>
+        <CategoryCards category={"technology"} />
       </section>
       <section id="Sports">
-        <CategoryCards category={'sports'}/>
+        <CategoryCards category={"sports"} />
       </section>
-      
-      
     </div>
   );
 }
--- src/components/CategoryCards.js
+++ src/components/CategoryCards.js
@@ -1,30 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import './styles.css';
-import Card from './Card';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import Card from "./Card";
+import axios from "axios";
 
 const CategoryCards = ({ category }) => {
-  const apiKey = '8389f4795e7f4535bfaeae36bb60a410';
+  const apiKey = "8389f4795e7f4535bfaeae36bb60a410";
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
     // Make an API request using Axios
     axios
-      .get(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}`)
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}`
+      )
       .then((response) => {
         if (response.data.articles) {
           setArticles(response.data.articles);
         }
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }, [category]);
 
   return (
-
     <div className="news-cards-default">
-        <div className='category-heading'>{category.toUpperCase()}</div>
+      <div className="category-heading">{category.toUpperCase()}</div>
       {articles.map((article, i) => (
         <Card key={i} article={article} />
       ))}
